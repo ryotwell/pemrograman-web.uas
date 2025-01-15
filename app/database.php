4f844c1ba4c9ae5d1e29ce48a42bc66936debfe7
Zulzario Zaeri <ryotwell@icloud.com>
2025-01-15 13:24:43 +0800

    + sqlite

--- app/database.php
+++ app/database.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Capsule\Manager;
 $capsule = new Manager();
 
 $capsule->addConnection([
-    'driver'    => 'mysql',
+    'driver'    => 'sqlite',
     'host'      => $_ENV['DB_HOST'],
     'database'  => $_ENV['DB_DATABASE'],
     'username'  => $_ENV['DB_USERNAME'],
